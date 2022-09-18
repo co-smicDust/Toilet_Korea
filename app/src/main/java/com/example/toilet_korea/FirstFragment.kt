b240d8afbaea14f19802c676295ffd0ac56404eb
jylee102 <101354244+jylee102@users.noreply.github.com>
2022-09-19 02:17:33 +0900

    소영님 작업

--- app/src/main/java/com/example/toilet_korea/FirstFragment.kt
+++ app/src/main/java/com/example/toilet_korea/FirstFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -33,9 +39,28 @@ class FirstFragment : Fragment(), MainActivity.onBackPressedListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //프래그먼트 새로고침 함수
+        fun refreshFragment(fragment: Fragment, fragmentManager: FragmentManager) {
+            var ft: FragmentTransaction = fragmentManager.beginTransaction()
+            ft.detach(fragment).attach(fragment).commit()
+        }
+
         val database: DatabaseReference = Firebase.database.reference
         val currentUser = Firebase.auth.currentUser
         val userRef = database.child("User").child(currentUser?.uid.toString())
+
+        val userListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val userName = dataSnapshot.value
+                if (userName != null) {
+                    getView()?.findViewById<TextView>(R.id.nickname)?.text = userName.toString()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        }
+        userRef.child("userNm").addValueEventListener(userListener)
 
         view.findViewById<Button>(R.id.join_button).setOnClickListener {
             val input = view.findViewById<EditText>(R.id.join_name).text
@@ -43,6 +68,8 @@ class FirstFragment : Fragment(), MainActivity.onBackPressedListener {
                 userRef.child("userNm").setValue(input.toString()).addOnSuccessListener {
                     Toast.makeText(this.context, "닉네임이 변경되었습니다.", Toast.LENGTH_SHORT).show()
                     input.clear()
+                    //프래그먼트 새로고침
+                    getFragmentManager()?.let { it1 -> refreshFragment(this, it1) }
                 }
             } else {
                 Toast.makeText(this.context, "새 닉네임을 입력해주세요.", Toast.LENGTH_SHORT).show()

b924f1ac70083d0cfb7d4b36ef8433e48bebb5af
jylee102 <101354244+jylee102@users.noreply.github.com>
2022-09-25 03:34:31 +0900

    Main에서 스레드 run, 데이터 전달

--- app/src/main/java/com/example/toilet_korea/MapFragment.kt
+++ app/src/main/java/com/example/toilet_korea/MapFragment.kt
@@ -7,9 +7,6 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,27 +15,23 @@ import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.maps.android.clustering.ClusterManager
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private var clusterManager: ClusterManager<MyItem?>? = null
+    private var clusterManager: ClusterManager<Toilet?>? = null
+
+    private var toilets: Collection<Toilet>? = null
 
     val database: DatabaseReference = Firebase.database.reference
 
     var rootView: View? = null
     var mapView: MapView? = null
-
-    var toilet: Toilet? = null
 
     // 런타임에서 권한이 필요한 퍼미션 목록
     val PERMISSIONS = arrayOf(
@@ -115,12 +108,13 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             googleMap.setOnCameraIdleListener(clusterManager)
             googleMap.setOnMarkerClickListener(clusterManager)
 
-            val clusterRenderer: MarkerClusterRenderer =
-                MarkerClusterRenderer(context, googleMap, clusterManager)
+            val clusterRenderer = MarkerClusterRenderer(context, googleMap, clusterManager)
             clusterManager!!.renderer = clusterRenderer
 
             // 권한이 있는 경우 맵 초기화
             initMap()
+
+            addMarkers()
         } else {
             // 권한 요청
             requestPermissions(PERMISSIONS, REQUEST_PERMISSION_CODE)
@@ -183,6 +177,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                     it.moveCamera(CameraUpdateFactory.newLatLngZoom(CITY_HALL, DEFAULT_ZOOM_LEVEL))
                 }
             }
+
             searching()
 
             googleMap?.setOnInfoWindowClickListener(GoogleMap.OnInfoWindowClickListener {
@@ -240,76 +235,17 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
-    //DB의 toilet reference
-    val toiletRef = database.child("toilet")
+    fun addMarkers(){
 
-    inner class ToiletThread : Thread() {
-        override fun run() {
-            handler.sendEmptyMessage(0)
+        if (arguments == null || !requireArguments().containsKey("toilet")) {
+
+            toilets = DataHolder.instance.data
+
+            if (toilets != null){
+                clusterManager?.addItems(toilets)
+                googleMap?.animateCamera(CameraUpdateFactory.zoomOut())
+            }
         }
-    }
-
-    fun getDistance(latitude: Double, longitude: Double): Int {
-        val currentLatLng = getMyLocation()
-
-        val locationA = Location("A")
-        val locationB = Location("B")
-
-        locationA.latitude = currentLatLng.latitude
-        locationA.longitude = currentLatLng.longitude
-        locationB.latitude = latitude
-        locationB.longitude = longitude
-
-        //에뮬레이터에서 현재 위치를 구할 수 없어 임의로 기본값 선택.
-        // 현재 위치 구할 수 있는 환경이라면, 위치 권한 거부했을 때의 기본값(현재 CITY_HALL)으로 바꿀 것 요망
-        return if (currentLatLng != LatLng(0.0, 0.0))
-            locationA.distanceTo(locationB).toInt()
-        else 0
-    }
-
-    // 스레드가 다운로드 받아서 파싱한 결과를 가지고 맵 뷰에 마커를 출력해달라고 요청
-    val handler: Handler = object : Handler(Looper.getMainLooper()) {
-        override fun handleMessage(msg: Message) {
-
-            toiletRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                @SuppressLint("PotentialBehaviorOverride")
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                    if (dataSnapshot.exists()) {
-                        // looping through to values
-                        for (i in dataSnapshot.children) {
-                            toilet = i.getValue(Toilet::class.java)!!
-
-                            if (toilet?.latitude != null && toilet?.longitude != null){
-                                val item = MyItem(toilet!!)
-                                clusterManager?.addItem(item)
-                            }
-
-                        }
-                        // 데이터 파싱 후 줌아웃을 함으로써 마커 표출 가능
-                        // 그렇지 않으면 사용자가 직접 줌아웃을 한 이후에 마커 확인 가능
-                        googleMap?.animateCamera(CameraUpdateFactory.zoomOut())
-                    }
-
-                }
-                override fun onCancelled(databaseError: DatabaseError) {}
-            })
-        }
-    }
-
-    var toiletThread: ToiletThread? = null
-
-    // 앱이 활성화될때 화장실 데이터를 읽어옴
-    @SuppressLint("PotentialBehaviorOverride")
-    override fun onStart() {
-        super.onStart()
-    }
-
-    // 앱이 비활성화 될때 백그라운드 작업 취소
-    override fun onStop() {
-        super.onStop()
-        toiletThread?.isInterrupted
-        toiletThread = null
     }
 
     fun searching(){
@@ -372,12 +308,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 bottomSheet.arguments = args
                 bottomSheet.show(parentFragmentManager, bottomSheet.tag)
             }
-
-        }else{
-            if (toiletThread == null) {
-                toiletThread = ToiletThread()
-                toiletThread!!.start()
-            }
         }
     }
+
 }
